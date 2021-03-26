9a1eaca7988c6b7db3069a02d3d50c91ba4c3064
Peter Johnson <74940976+pete6184@users.noreply.github.com>
2021-03-26 07:44:31 -0700

    added data from array to html

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,21 @@
 // from data.js
-var tableData = data;
+let tableData = data;
 
-// YOUR CODE HERE!
+// console.log(tableData)
+
+// Create references
+let tablebody = d3.select("tbody");
+let columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+// Input data into HTML
+let addData = (dataInput) => {
+    dataInput.forEach(sighting => {
+        let row = tablebody.append("tr");
+        columns.forEach(column => row.append("td").text(sighting[column])
+        )
+    });
+}
+
+addData(tableData);
+
+// Create filter for data
